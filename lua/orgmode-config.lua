f2c5c829facf537b6542d494f10f98156f9f3e4a
xueshi hu <hubachelar@gmail.com>
2022-02-21 13:05:05 +0800

    :bug: update orgmode config as suggested

--- lua/orgmode-config.lua
+++ lua/orgmode-config.lua
@@ -1,12 +1,4 @@
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
-    files = {'src/parser.c', 'src/scanner.cc'},
-  },
-  filetype = 'org',
-}
+require('orgmode').setup_ts_grammar()
 
 require'nvim-treesitter.configs'.setup {
   -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
