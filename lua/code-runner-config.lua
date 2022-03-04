093068e2829015fe8a234a30129c817f59f5438d
xueshi hu <hubachelar@gmail.com>
2022-03-04 18:06:00 +0800

    :gear: minor fix

--- lua/code-runner-config.lua
+++ lua/code-runner-config.lua
@@ -5,7 +5,7 @@ require('code_runner').setup {
   },
   filetype = {
     python = "python3 $fileName",
-    c = "cd $dir && clang -Wall -lpthread -g -std=c11 $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
+    c = "cd $dir && clang -Wall -lpthread -g -lm -std=c11 $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
     cpp = "cd $dir && clang++ -Wall -lpthread -g -std=c++17  $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
     html = "microsoft-edge $fileName",
     sh = "bash $file",
