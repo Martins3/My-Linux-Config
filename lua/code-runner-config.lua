9edf6a5389552a7312112e6a2fb31f135d75ca24
xueshi hu <hubachelar@gmail.com>
2022-03-24 00:18:37 +0800

    :bug: fix

--- lua/code-runner-config.lua
+++ lua/code-runner-config.lua
@@ -5,8 +5,8 @@ require('code_runner').setup {
   },
   filetype = {
     python = "python3 $fileName",
-    c = "cd $dir && clang -Wall -lpthread -g -lm -std=c11 $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
-    cpp = "cd $dir && clang++ -Wall -lpthread -g -std=c++17  $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
+    c = "cd $dir && clang -lpthread -g -lm $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
+    cpp = "cd $dir && clang++ -lpthread -g $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
     html = "microsoft-edge $fileName",
     sh = "bash $file",
     rust = "cargo run",
--- vim/misc.vim
+++ vim/misc.vim
@@ -94,4 +94,4 @@ let g:bookmark_no_default_key_mappings = 1
 " 默认不要折叠 markdown
 let g:vim_markdown_folding_disabled = 1
 
-let g:markdown_fenced_languages = ['html', 'python=py', 'bash=sh', 'c', 'cpp']
+let g:markdown_fenced_languages = ['html', 'python', 'bash=sh', 'c', 'cpp']
