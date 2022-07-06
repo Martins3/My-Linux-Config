07ab29b797838acac7a91ecd92eaf4715328fcb7
xueshi hu <hubachelar@gmail.com>
2022-07-06 20:57:54 +0800

    19

--- nixpkgs/system.nix
+++ nixpkgs/system.nix
@@ -24,7 +24,7 @@
   virtualisation.libvirtd = {
     enable = true;
     extraConfig = "--listen";
-  }
+  };
   # TMP_TODO 但是还是需要手动 sudo systemctl start libvirtd, 是因为需要重启的，如果启用你新的 service
   programs.dconf.enable = true;
   environment.systemPackages = with pkgs; [ virt-manager ];
