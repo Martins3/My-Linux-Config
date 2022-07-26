2c2d931bda4ea6ea1af0e52b3db4a6783ad25092
xueshi hu <hubachelar@gmail.com>
2022-07-26 11:39:57 +0800

    57

--- nixpkgs/system.nix
+++ nixpkgs/system.nix
@@ -64,4 +64,42 @@ in
 
   services.openssh.enable = true;
   networking.firewall.enable = false;
+
+  # https://gist.github.com/vy-let/a030c1079f09ecae4135aebf1e121ea6
+  services.samba = {
+    enable = true;
+
+    syncPasswordsByPam = true;
+    # You will still need to set up the user accounts to begin with:
+    # $ sudo smbpasswd -a yourusername
+
+    # This adds to the [global] section:
+    extraConfig = ''
+      browseable = yes
+      smb encrypt = required
+    '';
+
+    shares = {
+      homes = {
+        browseable = "no";  # note: each home will be browseable; the "homes" share will not.
+        "read only" = "no";
+        "guest ok" = "no";
+      };
+    };
+  };
+
+
+
+  # Curiously, `services.samba` does not automatically open
+  # the needed ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 445 139 ];
+  # networking.firewall.allowedUDPPorts = [ 137 138 ];
+
+
+
+  # To make SMB mounting easier on the command line
+  environment.systemPackages = with pkgs; [
+    cifs-utils
+  ];
+
 }
