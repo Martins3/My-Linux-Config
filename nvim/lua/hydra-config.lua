4ab508e2ca5a1ef37fc665c3abe28df3040b13e1
xueshi hu <hubachelar@gmail.com>
2022-09-07 23:16:55 +0800

    :broom: remove ccls.vim
    As it becomes useless after coc.nvim's latest update

--- nvim/lua/hydra-config.lua
+++ nvim/lua/hydra-config.lua
@@ -1,18 +1,6 @@
 local Hydra = require('hydra')
 
 Hydra({
-  name = 'Side scroll',
-  mode = 'n',
-  body = 'z',
-  heads = {
-    { 'h', '5zh' },
-    { 'l', '5zl', { desc = '←/→' } },
-    { 'H', 'zH' },
-    { 'L', 'zL', { desc = 'half screen ←/→' } },
-  }
-})
-
-Hydra({
   name = 'Window size',
   mode = 'n',
   body = 'ca',
