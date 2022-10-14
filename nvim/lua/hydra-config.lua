75787bad2f8b1473805121efdf46c06fd2309c19
xueshi hu <hubachelar@gmail.com>
2022-10-14 16:11:58 +0800

    :hugs: tune hydra.nvim configuration

--- nvim/lua/hydra-config.lua
+++ nvim/lua/hydra-config.lua
@@ -6,7 +6,7 @@ local hint = [[
  Adjust window size^^^^^^
  ^ ^ _k_ ^ ^
  _h_ ^ ^ _l_
- ^ ^ _j_ ^ ^     _<Esc>_ to exit
+ ^ ^ _j_ ^ ^   _<CR>_ or _<Esc>_ to exit
 ]]
 
 Hydra({
@@ -30,7 +30,7 @@ Hydra({
     { 'h', '5<C-w>>', },
     { 'l', '5<C-w><', { desc = ' h/l width' } },
     { '<Esc>', nil, { exit = true } },
-    { '<cr>', nil, { exit = true } },
+    { '<CR>', nil, { exit = true } },
   }
 })
 
