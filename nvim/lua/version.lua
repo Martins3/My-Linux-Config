2da1a0ed2c5e51d46f1e492c9690caf2de0ad01d
xueshi hu <hubachelar@gmail.com>
2022-10-22 01:05:29 +0800

    :taxi: optimize version checker

---
+++ nvim/lua/version.lua
@@ -0,0 +1,10 @@
+-- 检查 neovim 版本
+local actual_ver = vim.version()
+local nvim_ver = string.format("%d.%d.%d", actual_ver.major, actual_ver.minor, actual_ver.patch)
+
+if actual_ver.major == 0 then
+  if actual_ver.minor < 8 then
+    local msg = string.format("Please upgrade neovim version : expect %s, but got %s instead!\n", "0.8.0", nvim_ver)
+    vim.api.nvim_err_writeln(msg)
+  end
+end
