e1877d9e2c83c402011508e0fca6bb1cbc40d53b
xueshi hu <hubachelar@gmail.com>
2022-10-26 12:37:52 +0800

    :star2: rename config

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 cd ~ || exit 0
-git clone https://github.com/Martins3/My-Linux-config .dotfiles
-ln -sf ~/.dotfiles/conf/tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/conf/tigrc.conf ~/.tigrc
-ln -sf ~/.dotfiles/conf/kitty ~/.config/kitty
+if [[ ! -d ~/.dotfiles ]]; then
+  git clone https://github.com/Martins3/My-Linux-config .dotfiles
+fi
+mkdir -p ~/.config
+
+ln -sf ~/.dotfiles/nixpkgs ~/.config/nixpkgs
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
-ln -sf ~/.dotfiles/conf/wtf.yml ~/.config/wtf/config.yml
-ln -sf ~/.dotfiles/conf/zathurarc ~/.config/zathura/zathurarc
-ln -sf ~/.dotfiles/conf/starship.toml ~/.config/starship.toml
+ln -sf ~/.dotfiles/config/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/config/tigrc.conf ~/.tigrc
+ln -sf ~/.dotfiles/config/kitty ~/.config/kitty
+ln -sf ~/.dotfiles/config/wtf.yml ~/.config/wtf/config.yml
+ln -sf ~/.dotfiles/config/zathurarc ~/.config/zathura/zathurarc
+ln -sf ~/.dotfiles/config/starship.toml ~/.config/starship.toml
+
+# TMP_TODO tmux 的安装需要什么特殊的
+# TMP_TODO 会失败吗?
 
 # kitty is used to substitute alacritty
 # ln -sf ~/.dotfiles/conf/alacritty.yml ~/.alacritty.yml
