645e766d336a642d519d9a2b44d75b5a9542e2a9
bachelor hu <hubachelar@gmail.com>
2022-11-27 17:42:41 +0800

    :broom: no more trying on other window managers

--- nixpkgs/sys/gui.nix
+++ nixpkgs/sys/gui.nix
@@ -4,61 +4,6 @@
   services.xserver = {
     enable = true;
     xkbOptions = "caps:swapescape";
-    /* displayManager.gdm.enable = true; */
-    /* displayManager.gdm.wayland = false; */
-    /* desktopManager.gnome.enable = true; */
-
-    /**
-      desktopManager = {
-      xterm.enable = false;
-      };
-
-      displayManager = {
-      defaultSession = "none+i3";
-      };
-
-      windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-      dmenu #application launcher most people use
-      i3status # gives you the default i3 status bar
-      i3lock #default i3 screen locker
-      i3blocks #if you are planning on using i3blocks over i3status
-      ];
-      };
-    */
-
-    /*
-      # leftwm begin
-      desktopManager = {
-      xterm.enable = false;
-      };
-
-      displayManager = {
-      defaultSession = "none+leftwm";
-      };
-
-      windowManager.leftwm = {
-      enable = true;
-      };
-      # leftwm end
-    */
-
-    /*
-      # awesome begin
-      desktopManager = {
-      xterm.enable = false;
-      };
-
-      displayManager = {
-      defaultSession = "none+awesome";
-      };
-
-      windowManager.awesome = {
-      enable = true;
-      };
-      # awesome end
-    */
   };
 
   fonts = {
@@ -79,12 +24,5 @@
     fcitx5.addons = with pkgs; [
       fcitx5-rime
     ];
-
-    # 我现在用 ibus
-    /* enabled = "ibus"; */
-    /* ibus.engines = with pkgs.ibus-engines; [ */
-    /*   libpinyin */
-    /*   rime */
-    /* ]; */
   };
 }
