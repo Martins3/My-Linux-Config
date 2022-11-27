bed9b5e3a926cb27d3e2921e3a3b79042545bacc
bachelor hu <hubachelar@gmail.com>
2022-11-27 17:41:45 +0800

    :penguin: add wechat and wemeet

--- nixpkgs/home/gui.nix
+++ nixpkgs/home/gui.nix
@@ -4,21 +4,34 @@ let
     config.allowUnfree = true;
   };
 
+  wrapWine_nix = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/xieby1/nix_config/d57b5c4b1532eb5599b23c13ed063b2fa81edfa7/usr/gui/wrapWine.nix";
+    hash = "sha256-4vdks0N46J/n8r3wdChXcJbBHPrbTexEN+yMi7zAbKs=";
+  };
+  weixin_nix = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/xieby1/nix_config/d57b5c4b1532eb5599b23c13ed063b2fa81edfa7/usr/gui/weixin.nix";
+    hash = "sha256-ql6BE/IZBM31W/yqCayAdktcV2QZ/maVzwskybFZwz0=";
+  };
+  weixin = import weixin_nix {
+    wrapWine = import wrapWine_nix { inherit pkgs; };
+  };
 in
 {
 
   imports = [
     ./app/gnome.nix
-    /* ./gui/mime.nix */
   ];
 
   home.packages = with pkgs; [
+    weixin
     neovide
     unstable.clash
     unstable.wpsoffice
     unstable.sublime-merge
+    unstable.firefox
     drawio
     unstable.microsoft-edge-dev
+    nur.repos.linyinfeng.wemeet
     # wm
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
     rofi
@@ -40,7 +53,7 @@ in
       WantedBy = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = "${pkgs.clash.outPath}/bin/clash -d . -ext-ctl \"127.0.0.1:9090\"";
+      ExecStart = "${pkgs.clash.outPath}/bin/clash";
     };
   };
 
