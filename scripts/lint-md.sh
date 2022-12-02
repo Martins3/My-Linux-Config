f2d4b03bb983495e89b5041c60abf0c126491c39
bachelor hu <hubachelar@gmail.com>
2022-12-02 20:30:18 +0800

    :bug: minor update

--- scripts/lint-md.sh
+++ scripts/lint-md.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-lint-md ./docs/**/* -c .lintmdrc.json
+lint-md "docs/**/*" -c .lintmdrc.json --threads
 
 if [[ $? ]]; then
-  lint-md -f ./docs/**/* -c .lintmdrc.json
+  lint-md -f "docs/**/*" -c .lintmdrc.json --threads
 else
   exit 1
 fi
