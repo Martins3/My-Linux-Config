d87e74cf1e6ec91892bbb05deb6b586b6cbc6cf9
Martins3 <hubachelar@gmail.com>
2023-03-04 14:50:22 +0800

    fix: auto save and load bookmark (#101)

--- nvim/lua/usr/session.lua
+++ nvim/lua/usr/session.lua
@@ -4,10 +4,21 @@ require("persisted").setup {
 
 local group = vim.api.nvim_create_augroup("PersistedHooks", {})
 
+-- 不要让进入 vim 的时候光标在 nvim-tree 中，所以默认关闭 NvimTree
+-- 此外，需要自动保存 bookmarks
 vim.api.nvim_create_autocmd({ "User" }, {
   pattern = "PersistedSavePre",
   group = group,
   callback = function()
     vim.cmd "NvimTreeClose"
+    vim.cmd "BookmarkSave.vim-bookmarks"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "PersistedSavePost",
+  group = group,
+  callback = function()
+    vim.cmd "BookmarkLoad .vim-bookmarks"
   end,
 })
