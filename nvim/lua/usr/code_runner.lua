b10a442dc31d8e2f3da246d19e4689d06b57d375
Martins3 <hubachelar@gmail.com>
2023-04-01 15:47:37 +0800

    nu: revert nushell

--- nvim/lua/usr/code_runner.lua
+++ nvim/lua/usr/code_runner.lua
@@ -13,27 +13,13 @@ require('code_runner').setup {
   },
   filetype = {
     python = "python3 $file",
-    c = "cd $dir ; gcc -Wall -lpthread -fno-omit-frame-pointer -pg -g -lm $fileName -o $fileNameWithoutExt.out ; $dir/$fileNameWithoutExt.out",
-    cpp = "cd $dir ; g++ -std=c++20 -lpthread -g $fileName -o $fileNameWithoutExt.out  ; $dir/$fileNameWithoutExt.out",
+    c = "cd $dir && gcc -Wall -lpthread -fno-omit-frame-pointer -pg -g -lm $fileName -o $fileNameWithoutExt.out && $dir/$fileNameWithoutExt.out",
+    cpp = "cd $dir && g++ -std=c++20 -lpthread -g $fileName -o $fileNameWithoutExt.out  && $dir/$fileNameWithoutExt.out",
     sh = "bash $file",
     html = microsoft_edge(),
     rust = "cargo run",
     r = "Rscript $file",
     lua = "lua $file",
     nix = "nix eval -f $file",
-    nu = "if (nu-check $file ) == false { nu $file }"
   },
 }
-
-vim.api.nvim_create_autocmd('FileType', {
-	desc = 'auto check nushell code on save',
-
-	pattern = 'nu',
-	group = vim.api.nvim_create_augroup('check nushell', { clear = true }),
-	callback = function (opts)
-		vim.api.nvim_create_autocmd('BufWritePost', {
-			buffer = opts.buf,
-			command = 'RunCode'
-		})
-	end,
-})
