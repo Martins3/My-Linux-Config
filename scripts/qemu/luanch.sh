9c0be9b73b7a1cd2aa4c15e29a8dd853cd10ee0f
Martins3 <hubachelar@gmail.com>
2023-04-18 22:23:13 +0800

    kernel: rewrite qemu launch scripts

--- scripts/qemu/luanch.sh
+++ scripts/qemu/luanch.sh
@@ -1,45 +1,43 @@
 #!/usr/bin/env bash
 
 set -E -e -u -o pipefail
+# QEMU 的 -pidfile 在 QEMU 被 pkill 的时候自动删除的，但是如果 QEMU 是 segv 之类的就不会
+pidfile=/tmp/martins3-alpine/qemu-pid
 
-function martins3_prepare_qemu() {
-  # @todo 这种检测方式有问题
-  if pgrep qemu-system-x86; then
-    gum confirm "there's qemu process, maybe kill it firstly"
-  fi
+function close_qemu() {
+	if [[ -f $pidfile ]]; then
+		qemu=$(cat $pidfile)
+		if ps -p "$qemu" >/dev/null; then
+			gum confirm "Kill the machine?" && kill -9 "$qemu"
+		fi
+	fi
 }
 
 function debug_kernel() {
-  martins3_prepare_qemu
-  echo "good"
-  zellij run --close-on-exit -- /home/martins3/core/vn/docs/qemu/sh/alpine.sh -s
-  /home/martins3/core/vn/docs/qemu/sh/alpine.sh -k
-  gum confirm "Kill the machine?" && pkill qemu-system-x86
+	close_qemu
+	zellij run --close-on-exit -- "/home/martins3/core/vn/docs/qemu/sh/alpine.sh -s"
+	/home/martins3/core/vn/docs/qemu/sh/alpine.sh -k
+	close_qemu
 }
 
 function login() {
-  martins3_prepare_qemu
-  zellij run --close-on-exit -- /home/martins3/core/vn/docs/qemu/sh/alpine.sh -r
-  for i in x y; do
-    gum spin --spinner dot --title "waiting for the vm..." -- sleep 3
-    ssh -p5556 root@localhost
-    if [[ $? != 255 ]]; then
-      gum confirm "Kill the vm?" && pkill qemu-system-x86
-    fi
-    echo "try to connect to vm"
-  done
+	close_qemu
+	zellij run --close-on-exit -- "/home/martins3/core/vn/docs/qemu/sh/alpine.sh"
+	ssh -o '3' -p5556 root@localhost
+	close_qemu
 }
 
 while getopts "d" opt; do
-  case $opt in
-  d)
-    echo "---"
-    debug_kernel
-    exit 0
-    ;;
-  *)
-    login
-    exit 0
-    ;;
-  esac
+	case $opt in
+		d)
+			debug_kernel
+			exit 0
+			;;
+		*)
+			cat /home/martins3/.dotfiles/scripts/qemu/luanch.sh
+			exit 0
+			;;
+	esac
 done
+
+login
