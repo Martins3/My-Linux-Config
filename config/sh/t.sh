41617d339f19dda29b191580aa3d5c78befeb76a
Martins3 <hubachelar@gmail.com>
2023-06-05 21:30:07 +0800

    kernel: make trace more powerful

--- config/sh/t.sh
+++ config/sh/t.sh
@@ -2,11 +2,29 @@
 
 set -E -e -u -o pipefail
 
-check_return=false
-while getopts "r" opt; do
+cd "$(dirname "$0")"
+
+function finish {
+	if [[ $? -ne 0 ]]; then
+		echo "check what's happening"
+		echo "v $0"
+	fi
+}
+
+trap finish EXIT
+
+action="trace"
+while getopts "krh" opt; do
 	case $opt in
+		k)
+			action="kprobe"
+			;;
 		r)
-			check_return=true
+			action="kretprobe"
+			;;
+		h)
+			echo "usage : t funcname"
+			exit 0
 			;;
 		*)
 			exit 1
@@ -15,19 +33,30 @@ while getopts "r" opt; do
 done
 shift $((OPTIND - 1))
 
-for i in "$@"; do
-	echo "$i"
-done
-cd "$(dirname "$0")"
 if [[ $# -eq 0 ]]; then
-	echo "usage : t funcname"
-	exit 1
+	mkdir -p /tmp/martins3
+	bpftrace_cache=/tmp/martins3/bpftrace-cache
+	# shellcheck disable=SC2024
+	if [[ ! -f $bpftrace_cache ]]; then
+		sudo bpftrace -l >"$bpftrace_cache"
+	fi
+	entry=$(fzf <"$bpftrace_cache")
 fi
 
-set -x
-
-if [[ $check_return == true ]]; then
-	sudo bpftrace -e "kretprobe:${1} { printf(\"returned: %lx\\n\", retval); }"
-else
-	sudo bpftrace -e "kprobe:${1} {  @[kstack] = count(); }"
-fi
+scripts=""
+case "$action" in
+	kprobe)
+		scripts="kprobe:${1} { @[kstack] = count(); }"
+		;;
+	kretprobe)
+		scripts="kretprobe:${1} { printf(\"returned: %lx\\n\", retval); }"
+		;;
+	trace)
+		scripts="$entry { @[kstack] = count(); }"
+		;;
+	*)
+		exit 12
+		;;
+esac
+echo "sudo bpftrace -e $scripts"
+sudo bpftrace -e "$scripts"
