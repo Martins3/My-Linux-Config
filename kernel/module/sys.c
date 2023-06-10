3d7ce3ab269a48e4bef2d967f0eae7ba116fb071
Martins3 <hubachelar@gmail.com>
2023-06-10 11:06:44 +0800

    kernel: add sysfs example
    from https://github.com/sysprog21/lkmpg/blob/master/examples/hello-sysfs.c

---
+++ kernel/module/sys.c
@@ -0,0 +1,54 @@
+/*
+ * hello-sysfs.c sysfs example
+ */
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kobject.h>
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/sysfs.h>
+
+static struct kobject *mymodule;
+
+/* the variable you want to be able to change */
+static int myvariable = 0;
+
+static ssize_t myvariable_show(struct kobject *kobj,
+                               struct kobj_attribute *attr, char *buf) {
+  return sprintf(buf, "%d\n", myvariable);
+}
+
+static ssize_t myvariable_store(struct kobject *kobj,
+                                struct kobj_attribute *attr, char *buf,
+                                size_t count) {
+  sscanf(buf, "%du", &myvariable);
+  return count;
+}
+
+static struct kobj_attribute myvariable_attribute =
+    __ATTR(myvariable, 0660, myvariable_show, (void *)myvariable_store);
+
+
+int sysfs_init(void) {
+  int error = 0;
+
+  pr_info("mymodule: initialised\n");
+
+  // 在 /kernel/mymodule 中
+  mymodule = kobject_create_and_add("mymodule", kernel_kobj);
+  if (!mymodule)
+    return -ENOMEM;
+
+  error = sysfs_create_file(mymodule, &myvariable_attribute.attr);
+  if (error) {
+    pr_info("failed to create the myvariable file "
+            "in /sys/kernel/mymodule\n");
+  }
+
+  return error;
+}
+
+void sysfs_exit(void) {
+  pr_info("mymodule: Exit success\n");
+  kobject_put(mymodule);
+}
