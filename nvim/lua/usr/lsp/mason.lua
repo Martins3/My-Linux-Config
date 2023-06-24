79ed28ce6582db0f49694f4c15424bf83d33e4b5
Martins3 <hubachelar@gmail.com>
2023-06-24 18:25:32 +0800

    efm

--- nvim/lua/usr/lsp/mason.lua
+++ nvim/lua/usr/lsp/mason.lua
@@ -8,6 +8,7 @@ local servers = {
   -- "bashls",
   -- "jsonls",
   -- "yamlls",
+  "efm",
   "vimls",
   "ccls",
 }
---
+++ nvim/lua/usr/lsp/settings/efm.lua
@@ -0,0 +1,4 @@
+return {
+  filetypes = { "sh" },
+  command = "efm-langserver",
+}
