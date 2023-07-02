7e6a1d3be407e1f2e338e3de27947324fbffc9e1
Martin Hu <hubachelar@gmail.com>
2023-07-02 15:05:56 +0800

    nixos: setup configuration for kernel community

--- nixpkgs/system.nix
+++ nixpkgs/system.nix
@@ -192,8 +192,8 @@ in
     "ftrace=function"
     "ftrace_filter=amd_iommu_int_thread"
 
-    "processor.max_cstate=1"
-    "intel_idle.max_cstate=0"
+    # "processor.max_cstate=1"
+    # "intel_idle.max_cstate=0"
     # "amd_iommu=off"
     # "amd_iommu=pgtbl_v2"
     # "iommu=pt"
