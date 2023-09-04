0e90e6756af905bc814427a6d135dee4f7e423a0
martins3 <hubachelar@gmail.com>
2023-09-04 20:33:17 +0800

    nixos: microsoft-edge-dev is unstable

--- nixpkgs/sys/kernel.nix
+++ nixpkgs/sys/kernel.nix
@@ -7,7 +7,7 @@
   };
 
   boot.kernelParams = [
-    "transparent_hugepage=always"
+    "transparent_hugepage=never"
     # https://gist.github.com/rizalp/ff74fd9ededb076e6102fc0b636bd52b
     # 十次测量编译内核，打开和不打开的性能差别为 : 131.1  143.4
     # 性能提升 9.38%
