a64d01d76b6519ceb992ce35a8035b007b4d1f97
martins3 <hubachelar@gmail.com>
2023-10-30 22:10:39 +0800

    nixos: support 4.19 kernel

---
+++ nixpkgs/sys/kernel-419.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+{
+boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_4_19.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
+            sha256 = "0c9xxqgv2i36hrr06dwz7f3idc04xpv0a5pxg08xdh03cnyf12cx";
+      };
+      version = "4.19.297";
+      modDirVersion = "4.19.297";
+      };
+  });
+
+  boot.kernelParams = [
+    "mitigations=off"
+    "iommu=nopt"
+  ];
+
+}
