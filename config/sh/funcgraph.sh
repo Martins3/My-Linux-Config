fcee204744d5bf309dd9e32f2d44e0eae38444fa
martins3 <hubachelar@gmail.com>
2023-11-12 21:20:58 +0800

    kernel: add two helper bash for kernel ftrace

---
+++ config/sh/funcgraph.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -E -e -u -o pipefail
+
+mkdir -p /tmp/martins3
+bpftrace_cache=/tmp/martins3/available_filter_functions
+if [[ ! -s $bpftrace_cache ]]; then
+	# shellcheck disable=SC2024
+	sudo cat /sys/kernel/debug/tracing/available_filter_functions | tee $bpftrace_cache
+fi
+
+trap finish EXIT
+
+function finish {
+	echo nop | sudo tee /sys/kernel/debug/tracing/current_tracer
+	echo | sudo tee /sys/kernel/debug/tracing/set_event
+	echo | sudo tee /sys/kernel/debug/tracing/trace
+}
+
+finish
+
+if [[ $# -eq 0 ]]; then
+	entry=$(fzf <"$bpftrace_cache")
+else
+	echo "$*"
+	entry=$(fzf --query="$*" <"$bpftrace_cache")
+fi
+
+echo  function_graph | sudo tee /sys/kernel/debug/tracing/current_tracer
+echo "${entry%\[*\]}" | sudo tee /sys/kernel/debug/tracing/set_graph_function
+sudo cat /sys/kernel/debug/tracing/trace_pipe
---
+++ config/sh/tracepoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -E -e -u -o pipefail
+set -x
+
+mkdir -p /tmp/martins3
+tracepoint_cache=/tmp/martins3/tracepoint_cache
+if [[ ! -s $tracepoint_cache ]]; then
+	sudo cat /sys/kernel/debug/tracing/available_events | tee $tracepoint_cache
+fi
+
+trap finish EXIT
+
+function finish {
+	# TODO 才知道，不是清理掉 current_tracer ，而是清理掉 set_event 才对
+	echo nop | sudo tee /sys/kernel/debug/tracing/current_tracer
+	echo | sudo tee /sys/kernel/debug/tracing/set_event
+	echo | sudo tee /sys/kernel/debug/tracing/trace
+}
+
+finish
+
+if [[ $# -eq 0 ]]; then
+	entry=$(fzf <"$tracepoint_cache")
+else
+	echo "$*"
+	entry=$(fzf --query="$*" <"$tracepoint_cache")
+fi
+
+echo "${entry}" | sudo tee /sys/kernel/debug/tracing/set_event
+sudo cat /sys/kernel/debug/tracing/trace_pipe
