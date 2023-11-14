92369dd856aec66f4b43fbace2d8a064d01e924f
martins3 <hubachelar@gmail.com>
2023-11-14 21:25:37 +0800

    nixos: make build linux less frequently

---
+++ config/sh/build-linux.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -E -e -u -o pipefail
+
+source=~/core/linux
+target=~/core/linux-build
+
+A=/tmp/martins3/source_build_unstage.diff
+B=/tmp/martins3/source_build_staged.diff
+cd $source
+git diff >$A
+git diff --cached >$B
+
+A1=/tmp/martins3/target_build_unstage.diff
+B1=/tmp/martins3/target_build_staged.diff
+cd $target
+git diff >$A1
+git diff --cached >$B1
+
+function update() {
+	git reset --hard
+	[[ -s $A ]] && git apply $A
+	[[ -s $B ]] && git apply $B
+	cp ~/core/linux/.config ~/core/linux-build/
+}
+
+if diff $A $A1 &>/dev/null; then
+	if diff $B $B1 &>/dev/null; then
+		echo "Nothing changed, nothing to do 😹"
+		exit 0
+	fi
+fi
+
+update
