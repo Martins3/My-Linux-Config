c165f332fbe42beb73bad2c1185d86a7b53924ac
martins3 <hubachelar@gmail.com>
2024-03-09 13:42:36 +0800

    kernel: rcuwait

--- kernel/module/workqueue.c
+++ kernel/module/workqueue.c
@@ -4,6 +4,7 @@
 #include <linux/delay.h>
 #include "internal.h"
 
+// 一个 work 中的函数是不可以动态切换，使用 DECLARE_DELAYED_WORK 或者 INIT_DELAYED_WORK 之后无法修改
 static void mykmod_work_handler2(struct work_struct *w);
 static DECLARE_DELAYED_WORK(mykmod_work2, mykmod_work_handler2);
 static struct workqueue_struct *wq;
@@ -43,41 +44,61 @@ static void sleep_work(struct work_struct *w);
 static DECLARE_DELAYED_WORK(sleep_work_struct, sleep_work);
 static void sleep_work(struct work_struct *w)
 {
-	pr_info("sleep work start");
-	/* msleep(5000); */
-	pr_info("sleep work finished");
+	pr_info("sleep work start\n");
+	msleep(5000);
+	pr_info("sleep work finished\n");
 }
 static void no_sleep_work(struct work_struct *w);
 static DECLARE_DELAYED_WORK(no_sleep_work_struct, no_sleep_work);
 static void no_sleep_work(struct work_struct *w)
 {
-	pr_info("no sleep work finished");
+	pr_info("no sleep work finished\n");
 }
-// 一个 work 中的函数是不可以动态切换，使用 DECLARE_DELAYED_WORK 或者 INIT_DELAYED_WORK 之后无法修改
 
-static void test_sleep_in_workqueue(void)
+static void test_max_active_in_workqueue(void)
 {
 	if (!wq)
-		wq = create_singlethread_workqueue("my_workqueue");
-	// TODO
-	// 1. 不知道为什么第一个很快结束，但是第二个就是不执行
-	// 2. 按道理不应该睡眠，因为会阻碍其他人
-	//
-	// 是因为 singlethread 的原因吗？
+		wq = alloc_workqueue("my_workqueue", WQ_UNBOUND, 2);
 	queue_delayed_work(wq, &sleep_work_struct, HZ);
 	queue_delayed_work(wq, &no_sleep_work_struct, 2 * HZ);
+}
+
+// 并没有什么神奇的事情发生，简简单单的触发 softlock 而已
+static void dead_loop_work(struct work_struct *w);
+static DECLARE_DELAYED_WORK(dead_loop_work_struct, dead_loop_work);
+static void dead_loop_work(struct work_struct *w)
+{
+	for (;;)
+		cpu_relax();
+}
+static void test_dead_loop_in_workqueu(void)
+{
+	if (!wq)
+		wq = alloc_workqueue("my_workqueue", WQ_UNBOUND, 1);
+	queue_delayed_work(wq, &dead_loop_work_struct, 1 * HZ);
+}
+
+static void clear_workqueue(void)
+{
+	if (wq)
+		destroy_workqueue(wq);
 }
 
 /*
  * action 1,2 : 将任务挂到 system wq 上
  * action 3,4 : 将任务挂到自己构建的 wq 上
  *
- * INIT_DELAYED_WORK 是 DECLARE_DELAYED_WORK 的动态执行版本
+ * 1. INIT_DELAYED_WORK 是 DECLARE_DELAYED_WORK 的动态执行版本
+ * 2. queue_delayed_work 和 queue_work 实际上区别不大，只是加入队列前会睡眠一小会
+ * 3. max_active : 如果等于 1 ，那么即使第一个 work 睡眠了，第二个 work 也无法执行
  *
  */
 int test_workqueue(int action)
 {
 	switch (action) {
+	case 0:
+		clear_workqueue();
+		break;
 	case 1:
 		schedule_delayed_work(&mykmod_work, HZ);
 		break;
@@ -91,10 +112,10 @@ int test_workqueue(int action)
 		end_wq2();
 		break;
 	case 5:
-		test_sleep_in_workqueue();
+		test_max_active_in_workqueue();
 		break;
 	case 6:
-		/* test_dead_loop_in_workqueu(); */
+		test_dead_loop_in_workqueu();
 		break;
 	}
 	return 0;
