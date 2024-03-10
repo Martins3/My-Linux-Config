e94911c62a18824a16192ccf3493cd1d9fe18c46
martins3 <hubachelar@gmail.com>
2024-03-10 22:18:06 +0800

    kernel: rcuwait

--- kernel/module/access-once.c
+++ kernel/module/access-once.c
@@ -25,6 +25,7 @@ void test8(void);
  */
 void test9(void);
 void test10(void);
+void test11(void);
 
 // dead
 void test1(void)
@@ -140,10 +141,35 @@ void test10(void)
    * 0x0000000000000243 <+19>:    movl   $0x2222,0x0(%rip)        # 0x24d <test10+29>
    * 0x000000000000024d <+29>:    jmp    0x252
    */
-  // XXX 如果想要输出 0x3333 和 0x2222 ，这两个 WRITE_ONCE 都不可以省去
+  // XXX 如果想要输出 0x3333 和 0x2222 ，这两个 WRITE_ONCE 都不可以省去，
+  // 因为另外一个不会省去
 	WRITE_ONCE(access_once_test9, 0x3333);
+	/* access_once_test10 = 0x2222; */
 	WRITE_ONCE(access_once_test10, 0x2222);
 }
+
+/**
+ * XXX mb 的确可以防止指令重拍，但是 mb 和 READ_ONCE / WRITE_ONCE 是两个
+ * 维度的问题，只是有时候效果非常类似。
+ *
+ * 0x0000000000000270 <+0>:     endbr64
+ * 0x0000000000000274 <+4>:     call   0x279 <test11+9>
+ * 0x0000000000000279 <+9>:     movl   $0x2222,0x0(%rip)        # 0x283 <test11+19>
+ * 0x0000000000000283 <+19>:    movl   $0x2222,0x0(%rip)        # 0x28d <test11+29>
+ * 0x000000000000028d <+29>:    lock addl $0x0,-0x4(%rsp)
+ * 0x0000000000000293 <+35>:    movl   $0x3333,0x0(%rip)        # 0x29d <test11+45>
+ * 0x000000000000029d <+45>:    movl   $0x2222,0x0(%rip)        # 0x2a7 <test11+55>
+ * 0x00000000000002a7 <+55>:    jmp    0x2ac
+ */
+void test11(void)
+{
+	access_once_test9 = 0x2222;
+	access_once_test10 = 0x2222;
+  smp_mb();
+	access_once_test9 = 0x3333;
+	access_once_test10 = 0x2222;
+}
+
 
 int test_access_once(int action)
 {
--- kernel/module/rcuwait.c
+++ kernel/module/rcuwait.c
@@ -1,6 +1,9 @@
 #include <linux/rcuwait.h>
 #include "internal.h"
-
+/**
+  * 没有什么好资料，但是都是在 init commit 中
+  * 8f95c90ceb541a38ac16fec48c05142ef1450c25
+  */
 static struct rcuwait manager_wait = __RCUWAIT_INITIALIZER(manager_wait);
 
 static int state;
