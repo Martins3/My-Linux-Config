83542f1c6ac2c4cdd7c804fc1b937704c4d94d71
martins3 <hubachelar@gmail.com>
2024-04-03 18:31:38 +0800

    move junks out of the repo

--- shell/grep.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -E -e -u -o pipefail
-
-if cat /home/martins3/kernel/centos-3.10.0-1160.11.1-x86_64/drivers/scsi/scsi_lib.c | grep -q Copyright ; then
-  echo "found"
-else
-  echo "not found"
-fi
