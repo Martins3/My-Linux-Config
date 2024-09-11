6220f0f92a124d0859f70fb20e3114baa845077e
martins3 <hubachelar@gmail.com>
2024-09-11 10:11:38 +0800

    add glibc.nix
    Signed-off-by: martins3 <hubachelar@gmail.com>

--- scripts/nix/env/llvm.nix
+++ scripts/nix/env/llvm.nix
@@ -4,7 +4,7 @@ clangStdenv.mkDerivation {
   buildInputs = [
   ];
 }
-# 1. llvm-propject 应该是不需要使用什么外部依赖的，这个只是将
+# 1. llvm-propject 应该是不需要使用什么外部依赖的，甚至可以用 gcc 编译 clang
 # 2. 编译 cland 的方法
 # mkdir build && cd build
 # cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
