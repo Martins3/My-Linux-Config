3e09d2f8d5a50d24a3a3150a60ec7fc046aacaf4
Xueshi Hu <xueshi.hu@smartx.com>
2024-11-03 23:14:05 +0800

    sync spdk
    Signed-off-by: Xueshi Hu <xueshi.hu@smartx.com>

--- scripts/nix/env/spdk.nix
+++ scripts/nix/env/spdk.nix
@@ -1,55 +1,55 @@
 # 参考:
 # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/spdk/default.nix
-let
-  pkgs = import <nixpkgs> { };
-in
-pkgs.mkShell rec {
-  nativeBuildInputs = with pkgs.buildPackages; [
-      nasm
-      pkg-config
-      yasm
-      /* elfutils */
-      autoconf
-      autoconf-archive
-      automake
-      bison
-      cunit
-      dpdk
-      flex
-      fuse3
-      gettext
-      jansson
-      lcov
-      libaio
-      libbsd
-      libelf
-      libnl
-      libpcap
-      libtool
-      liburing
-      libuuid
-      ncurses
-      numactl
-      openssl
-      (python3.withPackages (p: with p; [
-                             setuptools
-                             pyelftools
-      ]))
-    zlib
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "spdk";
+
+  version = "24.09";
+
+  nativeBuildInputs = with pkgs; [
+    python3
+    python3.pkgs.pip
+    python3.pkgs.setuptools
+    python3.pkgs.wheel
+    python3.pkgs.wrapPython
+    python3.pkgs.pyelftools
+    pkg-config
+    ensureNewerSourcesForZipFilesHook
   ];
 
-  # @todo 不知道这个是什么原理?
-  NIX_CFLAGS_COMPILE = "-mssse3"; # Necessary to compile.
+  buildInputs = with pkgs; [
+    elfutils
+    cunit
+    dpdk
+    fuse3
+    jansson
+    libaio
+    libbsd
+    liburing
+    elfutils
+    libuuid
+    libpcap
+    libnl
+    numactl
+    openssl
+    ncurses
+    zlib
+    zstd
+  ];
+
+  propagatedBuildInputs  = with pkgs; [ 
+    python3.pkgs.configshell
+  ];
+
+  postPatch = ''
+    patchShebangs .
+  '';
+
+  enableParallelBuilding = true;
+
+  env.NIX_CFLAGS_COMPILE = "-mssse3"; # Necessary to compile.
   # otherwise does not find strncpy when compiling
-  NIX_LDFLAGS = "-lbsd";
+  env.NIX_LDFLAGS = "-lbsd";
 
-  buildInputs = with pkgs; [ ];
 }
-
-/**
-  * git clone https://github.com/spdk/spdk.git
-  * pushd spdk
-  * git submodule update --init
-  * ./configure --with-ublk
-  * make
-*/
