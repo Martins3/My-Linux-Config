8126c450ac3c4d3bd70bbb6c096d4884616e99e4
Xueshi Hu <xueshi.hu@smartx.com>
2024-11-08 20:23:05 +0800

    don't make it so complicated
    Signed-off-by: Xueshi Hu <xueshi.hu@smartx.com>

--- config/sh/tmux.sh
+++ config/sh/tmux.sh
@@ -1,40 +1,14 @@
 #!/usr/bin/env bash
 
 set -E -e -u -o pipefail
-function usage() {
-	echo "Usage :   [options] [--]
 
-    Options:
-    -h|help       Display this message
-    -n|note       Only open note session
-    -c|code       Only open code session
-    "
-}
+tmuxp load -d ~/.dotfiles/config/tmux-note.yaml
+tmuxp load -d ~/.dotfiles/config/tmux-code.yaml
 
-function note() {
-	tmuxp load -d ~/.dotfiles/config/tmux-note.yaml
-}
-
-function code() {
-	tmuxp load -d ~/.dotfiles/config/tmux-code.yaml
-}
-
-while getopts "cn" opt; do
-	case $opt in
-		c)
-			code
-			exit 0
-			;;
-		n)
-			note
-			exit 0
-			;;
-		*)
-			echo -e "\n  Option does not exist : OPTARG\n"
-			usage
-			;;
-	esac
-done
-
-code
-note
+if [[ -n $TMUX ]]; then
+	# 如果在 tmux 中，先退出
+	exit 0
+else
+  # 
+	exit 1
+fi
