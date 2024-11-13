33875e181dffade2e2e491a2fa7ef9c35daa1662
Xueshi Hu <xueshi.hu@smartx.com>
2024-11-13 11:21:47 +0800

    C-w is really slow on neovim
    Signed-off-by: Xueshi Hu <xueshi.hu@smartx.com>

--- nvim/lua/usr/hydra.lua
+++ nvim/lua/usr/hydra.lua
@@ -1,22 +1,11 @@
 local Hydra = require("hydra")
 
 -- 首先按 c a ，然后就可以使用 hjkl 来调整窗口大小
-
-local hint = [[
- Adjust window size^^^^^^
- ^ ^ _k_ ^ ^
- _h_ ^ ^ _l_
- ^ ^ _j_ ^ ^   _<CR>_ or _<Esc>_ to exit
-]]
-
 Hydra({
   name = "Adjust Window Size",
-  hint = hint,
   config = {
     color = "pink",
     invoke_on_body = true,
-    hint = {
-    },
     on_enter = function()
       vim.o.virtualedit = "all"
     end,
@@ -24,12 +13,10 @@ Hydra({
   mode = "n",
   body = "ca",
   heads = {
-    { "k",     "5<C-w>+" },
-    { "j",     "5<C-w>-", { desc = "j/k height" } },
-    { "h",     "5<C-w>>" },
-    { "l",     "5<C-w><", { desc = " h/l width" } },
-    { "<Esc>", nil,       { exit = true } },
-    { "<CR>",  nil,       { exit = true } },
+    { "j",     "<cmd>vertical resize +10<cr>" },
+    { "k",     "<cmd>vertical resize -10<cr>" },
+    { "<Esc>", nil,                           { exit = true } },
+    { "<CR>",  nil,                           { exit = true } },
   },
 })
 -- 当想要修改一块代码的缩进的时候，使用 < 或者 > ，然后使用 . 来重复，这是 vim 的默认行为。
