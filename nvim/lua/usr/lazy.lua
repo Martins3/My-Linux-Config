941d20a413ba85ef61252f433c317fd15c5d0b3b
Xueshi Hu <xueshi.hu@smartx.com>
2025-02-08 21:07:29 +0800

    deepseek
    Signed-off-by: Xueshi Hu <xueshi.hu@smartx.com>

--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -52,7 +52,7 @@ require("lazy").setup({
           win_width = 20,
           auto_preview = false,
           detail = true,
-          auto_close = false ,
+          auto_close = false,
           close_after_jump = true,
           keys = {
             toggle_or_jump = 'o',
@@ -186,6 +186,60 @@ require("lazy").setup({
     'mcauley-penney/visual-whitespace.nvim',
     config = true
   }, -- 在 visual mode 展示空白字符
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
   -- cppman
   {
     "madskjeldgaard/cppman.nvim",
