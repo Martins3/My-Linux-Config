6866bd15e4e665e2cfe2f9edf8e5210493322b99
martins3 <hubachelar@gmail.com>
2025-02-10 22:28:10 +0800

    disable avante as I can't get deepseek API key
    Signed-off-by: martins3 <hubachelar@gmail.com>

--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -188,6 +188,7 @@ require("lazy").setup({
   }, -- 在 visual mode 展示空白字符
   {
     "yetone/avante.nvim",
+    enabled = false,
     event = "VeryLazy",
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
@@ -206,40 +207,8 @@ require("lazy").setup({
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      -- "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        -- 把两个都移除 markdown 移除掉
-        opts = {
-          file_types = {  "Avante" },
-        },
-        ft = { "Avante" },
-      },
-    },
+      -- "stevearc/dressing.nvim",  -- 这个让 nvim-tree 的编辑有点不习惯
+    }
   },
   -- cppman
   {
