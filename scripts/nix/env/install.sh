1fab137bbc1b941305a7402a0633b41dfa1b7c16
martins3 <hubachelar@gmail.com>
2025-05-28 16:27:25 +0800

    💯
    Signed-off-by: martins3 <hubachelar@gmail.com>

--- scripts/nix/env/install.sh
+++ scripts/nix/env/install.sh
@@ -10,7 +10,11 @@ file=$(printf "%s\n" "${items[@]}" | fzf)
 
 gum confirm "Continue at [$(pwd)] with [$file]" || exit 0
 
-# 只能用 ln ，不可以用 ln -s ，
-# scripts/nix/env/rust-best.nix 需要加载当前目录中的 ./rust-toolchain.toml
-ln "$PROGDIR/$file" default.nix
+if [[ $(basename "$file") == rust-best.nix ]]; then
+	# 只能用 ln ，不可以用 ln -s
+	# scripts/nix/env/rust-best.nix 需要加载当前目录中的 ./rust-toolchain.toml
+	ln "$PROGDIR/$file" default.nix
+fi
+
+ln -sf "$PROGDIR/$file" default.nix
 echo "use nix" >>.envrc && direnv allow
