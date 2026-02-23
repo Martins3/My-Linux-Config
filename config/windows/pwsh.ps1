3154a1aa36dad942edff707b0f0a0cf6b2af4424
martins3 <hubachelar@gmail.com>
2026-02-23 10:30:36 +0800

    change prompt
    Signed-off-by: martins3 <hubachelar@gmail.com>

--- config/windows/pwsh.ps1
+++ config/windows/pwsh.ps1
@@ -28,6 +28,7 @@ Set-Alias c Clear-Host
 Set-Alias v nvim
 Set-Alias gs gitui
 
+$env:SHELL_ARCH = "🌳"
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 # oh-my-posh init pwsh | Invoke-Expression
